1274b38cfa0c49f208d19c010b6d7acc66afdfdb
MaxGraey <maxgraey@gmail.com>
2021-07-30 11:48:57 +0300

    more robust

--- assembly/rng.ts
+++ assembly/rng.ts
@@ -1,4 +1,4 @@
-
+const CHUNK_SIZE = 16;
 const POOL_SIZE = 256;
 const rnds8Pool = new Uint8Array(POOL_SIZE); // random values to pre-allocate
 let poolPtr = POOL_SIZE;
@@ -6,12 +6,12 @@ let poolPtr = POOL_SIZE;
 /**
  * Gets an Uint8Array(16) filled with random values
  */
-export default function rng(fn: (array: Uint8Array) => void = randomFill): Uint8Array {
-    if (poolPtr > POOL_SIZE - 16) {
-        fn(rnds8Pool);
+export default function rng(gen: (array: Uint8Array) => void = randomFill): Uint8Array {
+    if (poolPtr > POOL_SIZE - CHUNK_SIZE) {
+        gen(rnds8Pool);
         poolPtr = 0;
     }
-    return rnds8Pool.subarray(poolPtr, (poolPtr += 16));
+    return rnds8Pool.subarray(poolPtr, (poolPtr += CHUNK_SIZE));
 }
 
 /**
