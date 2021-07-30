d9036d9edd6be2a632c66a1ff36b30ea0ba2bab8
MaxGraey <maxgraey@gmail.com>
2021-07-30 11:45:54 +0300

    more

--- assembly/rng.ts
+++ assembly/rng.ts
@@ -1,11 +1,13 @@
-const rnds8Pool = new Uint8Array(256); // random values to pre-allocate
-let poolPtr = rnds8Pool.length;
+
+const POOL_SIZE = 256;
+const rnds8Pool = new Uint8Array(POOL_SIZE); // random values to pre-allocate
+let poolPtr = POOL_SIZE;
 
 /**
  * Gets an Uint8Array(16) filled with random values
  */
 export default function rng(fn: (array: Uint8Array) => void = randomFill): Uint8Array {
-    if (poolPtr > rnds8Pool.length - 16) {
+    if (poolPtr > POOL_SIZE - 16) {
         fn(rnds8Pool);
         poolPtr = 0;
     }
@@ -16,7 +18,7 @@ export default function rng(fn: (array: Uint8Array) => void = randomFill): Uint8
  * Default function to fill an array with random values
  */
 function randomFill(array: Uint8Array): void {
-    for (let i = 0, l = array.length; i < l; i++) {
+    for (let i = 0; i < POOL_SIZE; i++) {
         unchecked(array[i] = u8(Math.random() * 256));
     }
 }
